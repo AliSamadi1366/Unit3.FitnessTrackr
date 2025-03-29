709bad567d093ad54c6ccb911935b851ff155d8d
Ali Samadi <alisamadi.096@gmail.com>
2025-03-29 13:51:44 -0500

    useQuery is used for fetching list of activities from API then map over the list of activities and make a list and render it

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,20 @@
+import useQuery from "../api/useQuery";
 export default function ActivitiesPage() {
+  const {
+    data: activities,
+    loading,
+    error,
+  } = useQuery("/Activities", "activities");
+  if (loading) return <p>Loading...</p>;
+  if (error || !activities) return <p>{error || "No activities found."}</p>;
   return (
     <>
       <h1>Activities</h1>
-      <p>Imagine all the activities!</p>
+      <ul className="activities">
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.name}</li>
+        ))}
+      </ul>
     </>
   );
 }

b0da266b6777abbacad5d3a24ebcaae93437aef8
Ali Samadi <alisamadi.096@gmail.com>
2025-03-29 16:21:05 -0500

    check the token if exist then show the button click for the user,call useMuattion function, extract mutate function from useMutation and assign it to  deleteActivity function,so the deleteActivity now  delete an activity from list

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,10 +1,17 @@
+import useMutation from "../api/useMutation";
 import useQuery from "../api/useQuery";
+import { useAuth } from "../auth/AuthContext";
+
 export default function ActivitiesPage() {
   const {
     data: activities,
     loading,
     error,
   } = useQuery("/Activities", "activities");
+  const { mutate: deleteActivity } = useMutation("DELETE", "/Activities", [
+    "activities",
+  ]);
+  const { token } = useAuth();
   if (loading) return <p>Loading...</p>;
   if (error || !activities) return <p>{error || "No activities found."}</p>;
   return (
@@ -12,7 +19,16 @@ export default function ActivitiesPage() {
       <h1>Activities</h1>
       <ul className="activities">
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.name}</li>
+          <li key={activity.id}>
+            {activity.name}{" "}
+            {token ? (
+              <button onClick={() => deleteActivity(activity.id)}>
+                Delete
+              </button>
+            ) : (
+              ""
+            )}
+          </li>
         ))}
       </ul>
     </>

e64df90654ba921c0bcdf6318f4000118ea14d58
Ali Samadi <alisamadi.096@gmail.com>
2025-04-01 23:30:59 -0500

    make separate component function and import

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,73 +1,15 @@
-import useMutation from "../api/useMutation";
-import useQuery from "../api/useQuery";
 import { useAuth } from "../auth/AuthContext";
+import ActivitiesList from "./ActivitiesList";
+import Form from "./Form";
 
 export default function ActivitiesPage() {
-  const {
-    data: activities,
-    loading,
-    error,
-  } = useQuery("/Activities", "activities");
-  const { mutate: deleteActivity } = useMutation("DELETE", "/Activities", [
-    "id",
-  ]);
-
-  const { mutate: addActivity } = useMutation("POST", "/Activities", [
-    "activities",
-  ]);
-
-  const { token } = useAuth();
-  if (loading) return <p>Loading...</p>;
-  if (error || !activities) return <p>{error || "No activities found."}</p>;
-
-  const addnewActivity = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const newActivity = {
-      name: formData.get("name"),
-      description: formData.get("description"),
-    };
-    addActivity(newActivity);
-  };
+  const { token } = useAuth;
 
   return (
     <>
       <h1>Activities</h1>
-      <ul className="activities">
-        {activities.map((activity) => (
-          <li key={activity.id}>
-            {activity.name}{" "}
-            {token ? (
-              <button
-                onClick={() => {
-                  deleteActivity(activity);
-                }}
-              >
-                Delete
-              </button>
-            ) : (
-              ""
-            )}
-          </li>
-        ))}
-      </ul>
-      {token ? (
-        <form onSubmit={addnewActivity}>
-          <label>
-            Name:
-            <input name="name" type="text" />
-          </label>
-          <hr />
-          <label>
-            Description:
-            <input name="description" type="text" />
-          </label>
-          <hr />
-          <button type="submit">Add Activity</button>
-        </form>
-      ) : (
-        console.error("error")
-      )}
+      <ActivitiesList />
+       {token ? <Form /> : ""}
     </>
   );
 }
